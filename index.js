ff4d2b056d0baa85f62f2d009191aabc33ab22d7
EterLate24 <eterlate@gmail.com>
2022-04-12 18:20:08 +0600

    start

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+require('dotenv').config()
+const routes = require('./routes/mainRouter')
+const path = require('path')
+
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout:'main',
+    extname:'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(routes)
+
+async function start() {
+    try{
+        await mongoose.connect('mongodb+srv://eterlate:06122000@cluster0.jv3dk.mongodb.net/service',{
+            useNewUrlParser: true 
+        })
+        app.listen(PORT, ()=>{
+            console.log(`Server has been started on port ${PORT}`)
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+start()
+
